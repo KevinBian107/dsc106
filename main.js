f6c48ee6863df98d7fca1f6d2378c937d17223a7
Jack <wzhang3912@gmail.com>
2024-05-08 16:18:24 -0700

    change

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
 async function load() {
-    data = await d3.csv("/data/data.csv");
+    data = await d3.csv("clearned_data.csv");
+    console.log(data);
     return data
 }
 
-function drawGraph(data) {
+// function drawGraph(data) {
 
-    const svg = d3.select(vis);
+//     const svg = d3.select(vis);
 
-    d3.select(vis).html(null); 
+//     d3.select(vis).html(null);
 
-    // group the data
-    const gdp = d3.group(data, d => d.country);
+//     // group the data
+//     const gdp = d3.group(data, d => d.country);
+//     svg.selectAll(".line")
+//     .data(gdp)
+//     .join("path")
+//         .attr("fill", "none")
+//         //.attr("stroke", function(d){ return color(d[0]) })
+//         .attr("stroke-width", 1.5)
+//         .attr("d", function(d){
+//         return d3.line()
+//             .x(function(d) { return x(d.year); })
+//             .y(function(d) { return y(+d.n); })
+//             (d[1])
+//         })
+// }
 
-    svg.selectAll(".line")
-    .data(gdp)
-    .join("path")
-        .attr("fill", "none")
-        //.attr("stroke", function(d){ return color(d[0]) })
-        .attr("stroke-width", 1.5)
-        .attr("d", function(d){
-        return d3.line()
-            .x(function(d) { return x(d.year); })
-            .y(function(d) { return y(+d.n); })
-            (d[1])
-        })
-}
-
-
-drawGraph()
+data = load()
 
